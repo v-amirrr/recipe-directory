918e3d95dcf385e623a519efc849052dd4953acc
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-04-25 18:57:21 +0430

    build: default recipes in home page

---
+++ src/components/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { data } from '../data';
+
+const Recipe = ({data}) => {
+    return (
+        <>
+            <div>
+            
+            </div>
+        </>
+    );
+};
+
+export default Recipe;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,11 +4,13 @@ import styles from "./HomePage.module.css";
 
 import { data } from '../data';
 
+import Recipe from '../components/Recipe';
+
 const HomePage = () => {
     return (
         <>
                 <div className={styles.homePage}>
-                
+                    {data.map(item => <Recipe data={item} />)}
                 </div>
         </>
     );
