c8b723d0c37b680800dcdaef5fcd33ed3606d3fe
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-04-29 21:46:05 +0430

    chnage the style of build recipe page
    ALso added a new button in build recipe page called reset.
    You can use reset button when you want to delete everything you wrote.
    And noew when you create a recipe you'll be taken immediately to the home page.

--- src/components/BuildRecipe.js
+++ src/components/BuildRecipe.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import styles from "./RecipeDetails.module.css";
+import styles from "./BuildRecipe.module.css";
 
 import { data } from "../data";
 
 const BuildRecipe = () => {
+
+    let navigate = useNavigate();
 
     const [title, setTitle] = useState("");
     const [newIngredient, setNewIngredient] = useState("");
@@ -13,7 +16,6 @@ const BuildRecipe = () => {
     const [time, setTime] = useState();
 
     const addRecipe = e => {
-        e.preventDefault();
         data.push({
             id: data[data.length - 1].id + 1,
             title: title,
@@ -21,15 +23,25 @@ const BuildRecipe = () => {
             method: method,
             cookingTime: time
         });
+        resetInputs();
+        navigate("/");
     }
 
     const addIngredients = e => {
-        e.preventDefault();
         const ing = newIngredient.trim();
+        setNewIngredient("");
 
         if (ing && !ingredients.includes(ing)) {
             setIngredients([...ingredients, ing]);
         }
+    }
+
+    const resetInputs = e => {
+        setTitle("");
+        setNewIngredient("");
+        setIngredients([]);
+        setMethod("");
+        setTime("");
     }
 
     return (
@@ -37,23 +49,44 @@ const BuildRecipe = () => {
             <div className={styles.buildRecipe}>
                 <h1>Create a New Recipe</h1>
 
-                <form onSubmit={addRecipe}>
-                    <label>Recipe Title:</label>
-                    <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required />
+                <div>
+                    <div className={styles.formPart}>
+                        <label>Recipe Title:</label>
+                        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required />
+                    </div>
+
                     <hr />
-                    <label>Recipe Ingredients:</label>
-                    <input type="text" onChange={(e) => setNewIngredient(e.target.value)} value={newIngredient} required />
-                    <button onClick ={addIngredients}>Add</button>
-                    <div>{ingredients.map(item => <p key={Math.random()}>{item}</p>)}</div>
+
+                    <div className={styles.formPartIng}>
+                        <div className={styles.formPartIngOne}>
+                            <label>Recipe Ingredients:</label>
+                            <div>
+                                <input type="text" onChange={(e) => setNewIngredient(e.target.value)} value={newIngredient} required />
+                                <button onClick={addIngredients}>+</button>
+                            </div>
+                        </div>
+                        <div className={styles.ingredients}>{ingredients.map(item => <p key={Math.random()}>{item}</p>)}</div>
+                    </div>
+
                     <hr />
-                    <label>Recipe Method:</label>
-                    <textarea onChange={(e) => setMethod(e.target.value)} value={method} required />
+
+                    <div className={styles.formPart}>
+                        <label>Recipe Method:</label>
+                        <textarea onChange={(e) => setMethod(e.target.value)} value={method} required />
+                    </div>
+
                     <hr />
-                    <label>Cooking Time in Miunte:</label>
-                    <input type="number" onChange={(e) => setTime(e.target.value)} value={time} required />
-                    <hr />
-                    <button>Create The Recipe</button>
-                </form>
+
+                    <div className={styles.formPart}>
+                        <label>Cooking Time in Miunte:</label>
+                        <input type="number" onChange={(e) => setTime(e.target.value)} value={time} required />
+                    </div>
+
+                    <div className={styles.buttons}>
+                        <button onClick={resetInputs}>Reset</button>
+                        <button onClick={addRecipe}>Create The Recipe</button>
+                    </div>
+                </div>
             </div>
         </>
     );
